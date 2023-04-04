1d40b93bc465b0f860e797e6964bf553695f6932
Sharan Chitta <chitta.sharan@gmail.com>
2023-04-04 18:38:43 -0400

    Removed confluent-kafka

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json
 import os
 
 from dotenv import load_dotenv
-from producer import KafkaProducer
+# from producer import KafkaProducer
 from configuration import get_config
 import logging
 from configparser import ConfigParser
@@ -17,20 +17,20 @@ logger = logging.getLogger(__name__)
 
 SERVICE_BASE_DIR = os.path.dirname(__file__)
 
-environment = os.getenv("ENVIRONMENT")
-logger.info(f"Setting up Inertial Sensor service for environment [{environment}]")
-configuration = get_config(environment)
-
-confluent_properties_file = open("kafka_client_properties.ini")
-confluent_config_parser = ConfigParser()
-confluent_config_parser.read_file(confluent_properties_file)
-confluent_config = dict(confluent_config_parser['default'])
+# environment = os.getenv("ENVIRONMENT")
+# logger.info(f"Setting up Inertial Sensor service for environment [{environment}]")
+# configuration = get_config(environment)
+#
+# confluent_properties_file = open("kafka_client_properties.ini")
+# confluent_config_parser = ConfigParser()
+# confluent_config_parser.read_file(confluent_properties_file)
+# confluent_config = dict(confluent_config_parser['default'])
 
 
 class IRSensorService:
     def __init__(self):
         self.formatted_data = []
-        self.producer = KafkaProducer(confluent_config, configuration.get_kafka_ir_sensor_topic())
+        # self.producer = KafkaProducer(confluent_config, configuration.get_kafka_ir_sensor_topic())
 
     def start_server(self):
         with Lepton() as l:
@@ -38,24 +38,24 @@ class IRSensorService:
             cv2.normalize(a, a, 0, 65535, cv2.NORM_MINMAX)
             np.right_shift(a, 8, a)
             print(f"The thermal value is {np.uint8(a)}")
-            self.send(np.uint8(a))  # send to kafka
+            # self.send(np.uint8(a))  # send to kafka
 
-    def send(self, thermal_value):
-        logger.info(f"Producing message to Kafka topic: {configuration.get_kafka_ir_sensor_topic()}")
-        print(f"Producing message to Kafka topic: {configuration.get_kafka_ir_sensor_topic()}")
+    # def send(self, thermal_value):
+    #     logger.info(f"Producing message to Kafka topic: {configuration.get_kafka_ir_sensor_topic()}")
+    #     print(f"Producing message to Kafka topic: {configuration.get_kafka_ir_sensor_topic()}")
+    #
+    #     sample = {
+    #         "type": "calibration",
+    #         "data": {
+    #             "session": "f2cbf121-d0a8-4b5d-885b-59fd720605f9", # test session ID
+    #             "thermal": thermal_value
+    #         }
+    #     }
+    #     self.producer.produce(json.dumps(sample))
 
-        sample = {
-            "type": "calibration",
-            "data": {
-                "session": "f2cbf121-d0a8-4b5d-885b-59fd720605f9", # test session ID
-                "thermal": thermal_value
-            }
-        }
-        self.producer.produce(json.dumps(sample))
-
-    def send_loop_sample(self):
-        while True:
-            self.send(38)
+    # def send_loop_sample(self):
+    #     while True:
+    #         self.send(38)
 
 
 if __name__ == '__main__':
